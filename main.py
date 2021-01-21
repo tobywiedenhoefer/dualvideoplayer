b4d353dfd10ec026b6869d967a596d1618c2a4a6
Toby Wiedenhoefer <toby.wiedenh@gmail.com>
2021-01-21 00:08:31 -0600

    added first iframe

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def splash():
     return render_template('splash.html')
 
+@app.route('/play/video=<videoID>&sound=<soundID>')
+def dualplayers(videoID, soundID):
+    return render_template('dualplayer.html', ids={'videoid': str(videoID), 'soundid': str(soundID)})
+
 if __name__ == "__main__":
     app.run(
         debug=True
